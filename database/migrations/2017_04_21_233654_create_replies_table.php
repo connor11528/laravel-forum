7e3c4f95111cf8c77c4fa85d0d25a995e79f61f9
Connor Leech <connorleech@gmail.com>
2017-04-21 17:03:09 -0700

    add errythang before seeding the database - part 1

--- database/migrations/2017_04_21_233654_create_replies_table.php
+++ database/migrations/2017_04_21_233654_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateThreadsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateThreadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('thread_id');
             $table->integer('user_id');
-            $table->string('title');
             $table->text('body');
             $table->timestamps();
         });
@@ -29,6 +29,6 @@ class CreateThreadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('threads');
+        Schema::dropIfExists('replies');
     }
 }
