0a88d9802aba9ccff8982520270406af9a6e6112
Connor Leech <connorleech@gmail.com>
2017-04-30 15:32:41 -0700

    add database seeding for Replies and Threads

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,26 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Thread::class, function(Faker\Generator $faker){
+	return [
+		'user_id' => function(){
+			return factory('App\User')->create()->id;
+		},
+		'title' => $faker->sentence,
+		'body' => $faker->paragraph,
+
+	];
+});
+
+$factory->define(App\Reply::class, function(Faker\Generator $faker){
+	return [
+		'thread_id' => function(){
+			return factory('App\Thread')->create()->id;
+		},
+		'user_id' => function(){
+			return factory('App\User')->create()->id;
+		},
+		'body' => $faker->paragraph
+	];
+});
