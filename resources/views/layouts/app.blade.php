9a7334e6e0bd4723a43ade49e2ab83fae263e428
Connor Leech <connorleech@gmail.com>
2017-05-07 23:38:00 -0500

    signed in users can post replies to threads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href='/threads'>All Threads</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,23 @@
 
         </div>
     </div>
+
+    <div class='row'>
+        <div class="col-md-8 col-md-offset-2">
+        @if(auth()->check())
+            <form method='POST' action='{{ $thread->path() . "/replies" }}'>
+                {{ csrf_field() }}
+
+                <div class='form-group'>
+                    <textarea name='body' id='body' class='form-control' placeholder='Write your reply here' rows='5'></textarea>
+                </div>
+
+                <button type='submit' class='btn btn-default'>Post</button>
+            </form>
+        @else
+            <p>Please <a href='{{ route("login") }}'>sign in</a> to post a reply to this thread.
+        @endif
+        </div>
+    </div>
 </div>
 @endsection
