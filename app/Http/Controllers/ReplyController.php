42aa0fab0b6f43d147f85c124e13a433aa1a28be
Connor Leech <connorleech@gmail.com>
2017-06-07 10:52:42 -0700

    lesson 10 - validate replies

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -13,6 +13,10 @@ class ReplyController extends Controller
 	
     public function store($channelId, Thread $thread)
     {
+        $this->validate(request(), [
+            'body' => 'required'
+        ]);
+        
         $thread->addReply([
         	'body' => request('body'),
         	'user_id' => auth()->id()
