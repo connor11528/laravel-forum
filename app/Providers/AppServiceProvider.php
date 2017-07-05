29885d50a66b01af3c0440eecbeab099d77f5a38
Connor Leech <connorleech@gmail.com>
2017-07-05 12:59:44 -0700

    fix path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
 
         // For Heroku
         // https://laravel-news.com/laravel-5-4-key-too-long-error
-        Schema::defaultStringLength(191);
+        \Schema::defaultStringLength(191);
     }
 
     /**
